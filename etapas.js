309c6a3d2b604d3d320006d07d06938665938e51
Diener <dienezim@hotmail.com>
2020-12-10 14:57:00 -0300

    First Commit Urna Eletronica Base Yt-Bonieky-Lacerda

---
+++ etapas.js
@@ -0,0 +1,51 @@
+const etapas = [
+	{
+		function: 'VEREADOR',
+		numbers: 5,
+		candidates: [
+			{
+				number: 55555,
+				name: 'Asus',
+				partido: 'ANC',
+				imagens: [
+					{ url: 'media/55555.png', legenda: 'VEREADOR', small: false },
+				],
+			},
+
+			{
+				number: 12345,
+				name: 'Lenovo',
+				partido: 'ABC',
+				imagens: [
+					{ url: 'media/12345.jpeg', legenda: 'VEREADOR', small: false },
+				],
+			},
+		],
+	},
+	{
+		function: 'PREFEITO',
+		numbers: 2,
+		candidates: [
+			{
+				number: 13,
+				name: 'ARNO',
+				partido: 'ABC',
+				vice: 'ADIDAS',
+				imagens: [
+					{ url: './media/13.jpg', legenda: 'PREFEITO', small: false },
+					{ url: './media/13_2.jpg', legenda: 'VICE-PREFEITO', small: true },
+				],
+			},
+			{
+				number: 32,
+				name: 'NIKE',
+				partido: 'CCC',
+				vice: 'LG',
+				imagens: [
+					{ url: './media/32.jpg', legenda: 'PREFEITO', small: false },
+					{ url: './media/32_2S.jpg', legenda: 'VICE-PREFEITO', small: true },
+				],
+			},
+		],
+	},
+];
---
+++ script.js
@@ -0,0 +1,123 @@
+const position = document.querySelector('.text-voting span');
+const infoCandidate = document.querySelector('.info-candidate');
+const footer = document.querySelector('.footer');
+const photos = document.querySelector('.photo-candidates');
+const numbers = document.querySelector('.number-candidate');
+
+let currentStage = 0;
+let i = 0;
+let isConfirmed = false;
+let isWhite = false;
+const timeout = 5000;
+
+let numberHTML = '';
+let stage = '';
+
+const clearScreen = () => {
+	footer.style.display = 'block';
+	photos.innerHTML = '';
+	position.innerHTML = '';
+	infoCandidate.innerHTML = '';
+	footer.style.display = 'none';
+	numbers.innerHTML = '';
+};
+
+const setStage = () => {
+	stage = etapas[currentStage];
+	numberHTML = '';
+	position.innerHTML = stage.function;
+	infoCandidate.innerHTML = '';
+	footer.style.display = 'none';
+	photos.innerHTML = '';
+	numbers.innerHTML = numberHTML;
+	numbers.innerHTML += '<div class="number-display pisca"></div>';
+};
+
+const handleClick = (number) => {
+	if (numberHTML.length < stage.numbers) {
+		divPisca = document.querySelector('.number-display.pisca');
+		divPisca.innerHTML = number;
+		numberHTML = `${numberHTML}${number}`;
+		divPisca.classList.remove('pisca');
+		if (numberHTML.length < stage.numbers)
+			numbers.innerHTML += '<div class="number-display pisca"></div>';
+		else {
+			updateInterface();
+		}
+	}
+};
+
+const updateInterface = () => {
+	var candidate = stage.candidates.filter((item) => {
+		if (item.number == numberHTML) return true;
+		else return false;
+	})[0];
+	if (candidate) {
+		footer.style.display = 'block';
+		infoCandidate.innerHTML = `NOME:${candidate.name} <br/>PARTIDO:${candidate.partido}`;
+		let divPhotos = '';
+		for (let i in candidate.imagens) {
+			divPhotos += `<div class="image">
+			<img src="${candidate.imagens[i].url}" alt="" />
+			${candidate.imagens[i].legenda}
+		</div>`;
+		}
+		photos.innerHTML = divPhotos;
+	} else {
+		footer.style.display = 'block';
+		infoCandidate.innerHTML = `<div class="alert-big pisca">VOTO NULO</div>`;
+	}
+};
+
+const handleClickWhite = () => {
+	if (numberHTML.length <= 0) {
+		isWhite = true;
+		footer.style.display = 'block';
+		numbers.innerHTML = '';
+		infoCandidate.innerHTML = `<div class="alert-big pisca">VOTO EM BRANCO</div>`;
+	} else {
+		alert('Não pode conter números nos espaços');
+	}
+	console.log(`Clicou em BRANCO`);
+};
+
+const handleClickCorrect = () => {
+	setStage();
+	console.log(`Clicou em CORRIGE`);
+};
+
+const Candidate = () => {
+	api = stage.candidates.filter((candidate) => {
+		if (candidate.number === parseInt(numberHTML)) {
+			return `${candidate.name}  PARTIDO: ${candidate.partido}`;
+		}
+	})[0];
+	const { name, partido } = api;
+	return `\n${name}\nPartido: ${partido}`;
+};
+
+const handleClickConfirm = () => {
+	if (isWhite) {
+		clearScreen();
+		alert('Voto confirmado como Branco');
+		console.log('Voto confirmado como Branco');
+	} else if (numberHTML.length == stage.numbers) {
+		clearScreen();
+		alert(`Votou no Candidato ${Candidate()}`);
+
+		setTimeout(() => {
+			currentStage++;
+			if (currentStage < etapas.length) {
+				setStage();
+			} else {
+				infoCandidate.innerHTML = `<div class="alert-big">FIM</div>`;
+				setTimeout(() => {
+					currentStage = 0;
+					setStage();
+				}, timeout * 2);
+			}
+		}, 500);
+	}
+	console.log(`Clicou em CONFIRMA`);
+};
+setStage();
